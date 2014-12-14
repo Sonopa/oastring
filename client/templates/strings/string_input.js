7073be5a32349bddc8473b001ec3c55a66947282
Jussi Mertanen <juzax@hotmail.com>
2014-12-14 21:24:39 +0200

    Project to github

---
+++ client/templates/strings/string_input.js
@@ -0,0 +1,23 @@
+Template.stringInput.helpers({
+  string: function () {
+    return Session.get("string");
+  },
+  length: function () {
+    return Session.get("string").length;
+  },
+  words: function () {
+    if (Session.get("string").length == 0) {
+      return 0;
+    }
+    return Session.get("string").split(/\s+\b/).length;    
+  },
+  reverse: function () {
+    return Session.get("string").split('').reverse().join('');
+  }
+});
+
+Template.stringInput.events({
+  'click button': function (event, template) {
+    Session.set("string", document.getElementById('string').value);
+  }
+});
